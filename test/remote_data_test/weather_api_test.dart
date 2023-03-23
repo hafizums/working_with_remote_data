cc5a78326e3ff39a55792774030cb534cc3d7e93
Hafiz Arujin <al.hafiz@ums.edu.my>
2023-03-23 10:40:05 +0800

    init

---
+++ test/remote_data_test/weather_api_test.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
+
+class DevHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+Future<void> main() async {
+  HttpOverrides.global = DevHttpOverrides();
+}
